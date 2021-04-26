cdcaa695080294521adf89a1109fdd075b80ebd1
winterpf <>
2021-04-26 07:37:17 +0000

    sugar

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 class Main {
   public static void main(String[] args) {
-    Sugar mySugar = new Sugar(5, 10);
-
-    mySugar 
+    Sugar tree=new Sugar ("Tree");
+    tree.cutShapes(tree.getShape(),10);
+    tree.bakeCookies(350,12);
+    tree.decorate();
+    Sugar aCookie = new Sugar();
+    aCookie.decorate();
   }
 }
--- Sugar.java
+++ Sugar.java
@@ -1,3 +1,43 @@
 class Sugar extends Cookie {
-  private int 
+  String shape;
+  boolean decorated;
+  
+  Sugar() 
+  {
+    shape = "";
+    decorated = false;
+    }
+    Sugar(String a) {
+      shape = a;
+      decorated = false;
+    }
+    String getShape() {
+      return shape;
+    }
+    void changeShape (String) {
+      shape = a;
+    }
+    void cutShape(String a, int b) {
+      System.out.println(b + "cookies were cut into " + a);
+      super.setNumCookie(b);
+    }
+
+    void decorated() {
+      if(super.isReady()==true){
+        decorated = true;
+        System.out.println("The cookies were cut into " + a);
+        super.setNumCookie(b);
+      }
+    }
+    void decorate() {
+      if(super.isReady()== true)
+      {
+        decorated = true;
+        System.out.println("The cookies were decorated!");
+      }
+    
+      else{
+      System.out.println("Make sure you bake the cookies first!");
+    }
+  }
 }
